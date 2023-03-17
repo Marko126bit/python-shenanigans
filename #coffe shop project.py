4e464f3f395f9468403483e702fbfcc8631ec7df
Marko126bit <128129906+Marko126bit@users.noreply.github.com>
2023-03-17 09:06:46 +0100

    Initial commit

---
+++ #coffe shop project.py
@@ -0,0 +1,19 @@
+#coffe shop project
+
+#lets make our coffe barista
+print("hello welcome to our coffe shop") 
+
+name = input("what is youre name sir?\n")
+
+print("hello " + name +  " ,Thank you so much for coming here.\n\n\n")
+#name = "customer"
+#print (name)
+#name = "crackhead"
+#print("crackhead")
+menu = "coffe,latte,cookie"
+
+print(name + ", what would you like to order?,here is our menu\n" + menu)
+
+order = input()
+
+print("sounds very good " + name + " i will try that  " + order + " ready for you in a moment")
