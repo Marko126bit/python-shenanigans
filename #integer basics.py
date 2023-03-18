c84b657b1d06af6a419578ea3f848742b93d456c
Marko126bit <128129906+Marko126bit@users.noreply.github.com>
2023-03-18 14:54:47 +0100

    Add files via upload

---
+++ #integer basics.py
@@ -0,0 +1,22 @@
+#integer basics
+#Marko(string)
+name = "Marko"
+   #23(integer) 
+age = 23
+#skracenica za print moze biti print = bilo sta(VAZI I ZA NEKE DRUGE KOMANDE KAO SKRACENICA NA VARIABLE)
+p = print
+
+p (name)
+p (age)
+#float=bilo koji broj sa zarezom
+age2 = 23.56
+p (age2)
+#primeri integera
+math =2+2/2*2+8-4
+p(2 + 2)
+p(2 * 2)
+p(8 - 4)
+p(4 / 4)
+#primer spajanja integera
+results = age + age2 + math
+p (results)
