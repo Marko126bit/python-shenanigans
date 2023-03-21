983bc8a6222fe2ff613d38b9e29604c45b08af76
Marko126bit <128129906+Marko126bit@users.noreply.github.com>
2023-03-21 17:05:46 +0100

    Add files via upload

---
+++ #if else statement.py
@@ -0,0 +1,10 @@
+
+name = input ("who are you?\n")
+last_name = input("and the last name?\n")
+
+if name=="Marko" and last_name== "Manojlovic":
+    print("nice, good job")
+else:
+    print("wrong see ya later")
+    exit()
+
