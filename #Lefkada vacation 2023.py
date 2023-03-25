e2f457a74156ac40cb0c9b8a8a851072f0d1e722
Marko126bit <128129906+Marko126bit@users.noreply.github.com>
2023-03-25 19:45:49 +0100

    Add files via upload

---
+++ #Lefkada vacation 2023.py
@@ -0,0 +1,18 @@
+#Lefkada vacation 2023
+
+#LIST []             TEST = 0
+lefkada_accesories = ["test","e - scooter", "vegeta" , "quad bike" , "good tomatoes " , "laptop" , "powerbank" , "sun protector"
+, "luggage" , "ball" , "mythos beer" ]
+
+#print(lefkada_accesories)
+
+lefkada_location = ["Egremni" , 10 , 27.5 , True]
+
+me = lefkada_accesories [1 and 9]
+print(me)
+
+Dejan = lefkada_accesories [10 and 5] 
+print(Dejan)
+
+Milos = lefkada_accesories [5 and 6 ] 
+print(Milos)
