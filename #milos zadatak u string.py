7d6b70972153b7657660b5d77f8aa8db49bfbe97
Marko126bit <128129906+Marko126bit@users.noreply.github.com>
2023-03-30 00:02:49 +0200

    Add files via upload

---
+++ #milos zadatak u string.py
@@ -0,0 +1,18 @@
+#milos zadatak
+
+string1 =("3,9,13,4,42")
+
+print(string1)
+
+list1 = string1 =[3,9,13,4,42]
+
+res = []
+
+for i in list1:
+    res.append (i * i)
+    
+for j in res:
+    print( j , end= " ")
+
+
+print(j)
